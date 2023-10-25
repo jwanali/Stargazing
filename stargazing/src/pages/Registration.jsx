4d31565211933d7de8891f48a87fded281c8665f
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-10-25 09:54:24 -0500

    Add styling to signup page

--- stargazing/src/pages/Registration.jsx
+++ stargazing/src/pages/Registration.jsx
@@ -1,27 +1,28 @@
 import React from "react";
-import  ../Registration.css;
+import  "../styles/Registration.css";
 export default function Registration() {
   return (
     <form>
-      <div className="">
+      <div className="signup">
+        <h1>Sign up</h1>
         <label>
           <p>username</p>
-          <input type="text" />
+          <input type="text" placeholder="Username"/>
         </label>
         <label>
           <p>Email</p>
-          <input type="text" />
+          <input type="text" placeholder="Email"/>
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" placeholder="Password" />
         </label>
         <label>
           <p>Confirm Password</p>
-          <input type="password" />
+          <input type="password" placeholder="Confirm Password" />
         </label>
         <div>
-          <button type="submit">Register</button>
+          <button type="submit">Confirm</button>
         </div>
       </div>
     </form>
