f5d88e97b2171d04aed7940927197ce5272d33dc
Lauren <laurenkaitlyn@gmail.com>
2023-10-31 18:04:34 -0400

    added a page

--- stargazing/src/components/Experience.jsx
+++ stargazing/src/components/Experience.jsx
@@ -1,16 +1,18 @@
 import { OrbitControls, ScrollControls } from "@react-three/drei";
 import { Earth } from "./Earth";
 import { Overlay } from "./Overlay";
+import Events from "./Events";
 
 export function Experience() {
   return (
     <>
       <ambientLight intensity={1} />
       <OrbitControls enableZoom={false} />
-      <ScrollControls pages={2} damping={0.25}>
+      <ScrollControls pages={3} damping={0.25}>
         <Overlay />
         <Earth />
       </ScrollControls>
+   
     </>
   );
-};
+}
