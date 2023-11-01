435d5e83a09887b72e38459bcdc166e9923f4f28
jwanali <ciwan_ali@hotmail.com>
2023-10-31 21:14:50 -0500

    merged

--- stargazing/src/App.jsx
+++ stargazing/src/App.jsx
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import './components/Weather'
-import Weather_api from './hook/weather_api';
-import Weather from './components/Weather';
-import Login from './components/Login';
-import { useState } from 'react';
-//ip = JSON.parse(body).ip;
-
-function App() {
-  const {
-    weather,
-    SET_WEATHER_DATA,
-    setSidePeek,
-    sidePeek,
-    // show_weather,
-    // set_show_weather,
-    // users,
-    // setUsers,
-    // setLogin
-
-  } = Weather_api()
- 
-  return (
-    <div className="App">
-      <header className="App-header">
-        Stargazing
-        <button onClick={() => {
-          // set_show_weather();
-        if (!sidePeek) {
-          setSidePeek(true);
-          SET_WEATHER_DATA();
-         
-        } else {
-          setSidePeek(false);
-         
-        };
-       
-        
-        }} >show_weather</button>
-        
-        {sidePeek && <Weather weather={weather} SET_WEATHER_DATA={SET_WEATHER_DATA}  ></Weather>}
-      </header>
-    
-      {/* <Login setLogin={setLogin} users={users} setUsers={setUsers}></Login> */}
-    </div>
-=======
 import "./App.css";
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
@@ -65,7 +17,6 @@ function App() {
         </Suspense>
       </Canvas>
     </>
->>>>>>> a52c2bff888aa4983c75a15c66a0311bdf8bbd30
   );
 }
 
