59f33740384898a1ffc26ac77ed0a6138dd12d9e
Lauren <laurenkaitlyn@gmail.com>
2023-11-04 11:53:21 -0400

    display info to client

--- stargazing/src/components/Events/index.jsx
+++ stargazing/src/components/Events/index.jsx
@@ -5,58 +5,73 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 import Weather_api from "../../hook/weather_api";
 import Weather from "../Weather";
-import Eclipse from "./eclipses";
-import Planets from "./planets";
-import Moon from "./moon";
-import Sun from "./sun";
 
 export default function Events(props) {
-  const { Content, Sider } = Layout;
   const [show, setShow] = useState(false);
 
-
   const contentList = {
-    tab1: <p>Content 1 </p>,
+    Moon: (
+      <div className="moon">
+        <h2>Moon Phase: Waning Gibbous</h2>
+        <h2>Next Phase: Nov 5, 2023 - Third Quarter</h2>
+        <h2>Moonrise Today: 8:43pm</h2>
+        <h2>Moonset Today: 12:27pm</h2>
+      </div>
+    ),
+    Sun: (
+      <div label="Sun">
+        <h2>Daylight Hours: 10 hours, 16 minutes</h2>
+        <h2>Sunrise Today: 7:57am</h2>
+        <h2>Sunset Today: 6:13pm</h2>
+        <h2>Next Solstice: Dec 21, 2023 10:27pm</h2>
+      </div>
+    ),
+    Planets: (
+      <div label="Planets">
+        <h1> Visible tonight </h1>
+        <h2>Mercury: Until Wed 6:29 pm</h2>
+        <h2>Venus: From Thu 3:58 am</h2>
+        <h2>Mars: Until Wed 6:25 pm</h2>
+        <h2>Jupiter: From Wed 6:16 pm</h2>
+        <h2>Saturn: Until Thu 2:02 am</h2>
+        <h2>Uranus: From Wed 6:40 pm</h2>
+        <h2>Neptune: Until Thu 4:12 am</h2>
+      </div>
+    ),
   };
 
   const tabList = [
     {
-      key: 1,
-      tab: 1,
+      key: "Moon",
+      tab: "Moon",
       label: "Moon",
     },
     {
-      key: 2,
-      tab: 2,
+      key: "Sun",
+      tab: "Sun",
       label: "Sun",
     },
     {
-      key: 3,
-      tab: 3,
-      label: "Eclipse",
-    },
-    {
-      key: 4,
-      tab: 4,
+      key: "Planets",
+      tab: "Planets",
       label: "Planets",
     },
   ];
 
-  const [activeTabKey1, setActiveTabKey1] = useState(1);
+  const [activeTabKey1, setActiveTabKey1] = useState('Moon');
   const onTab1Change = (key) => {
     setActiveTabKey1(key);
   };
 
   return (
-
-    <div style={{visibility: props.status ? "visible" : "hidden"
-        }}>
-    <h1 className="event-header" >Here is what you can see on date in city</h1>
-    <Card
+    <div className="events-card" style={{ visibility: props.status ? "visible" : "hidden" }}>
+      <h1 className="event-header">Here is what you can see on date in city</h1>
+      <Card
         style={{
           width: "100%",
         }}
         className="box"
+     
         tabList={tabList}
         activeTabKey={activeTabKey1}
         onTabChange={onTab1Change}
