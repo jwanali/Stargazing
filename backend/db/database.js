01a790ad696ef634d41ca794454da12346c27f08
jwanali <ciwan_ali@hotmail.com>
2023-11-04 16:32:41 -0500

    implementing  CRUD for events

--- backend/db/database.js
+++ backend/db/database.js
@@ -33,16 +33,38 @@ const add_event = function (event) {
       [event.event_name, event.date, event.description]
     )
     .then((result) => {
+      console.log(result)
      console.log('event added');
     })
     .catch((err) => {
       console.log(err.message);
     });
 }
-
+const delete_event = function (id) {
+  return db
+    .query(`DELETE FROM events WHERE id = $1 RETURNING *; `,[id])
+    .then((result) => {
+      console.log('event deleted');
+     })
+     .catch((err) => {
+       console.log(err.message);
+     });
+};
+const update_event = function (id,event) {
+  return db
+    .query(`UPDATE events SET event_name = $2, date = $3, description =$4  WHERE id = $1;`,[id, event.event_name, event.date, event.description])
+    .then((result) => {
+      console.log('event updated');
+     })
+     .catch((err) => {
+       console.log(err.message);
+     });
+}
 
 module.exports = {
   signupUsers,
   getUserWithEmail,
-  add_event
+  add_event,
+  delete_event,
+  update_event
 };
