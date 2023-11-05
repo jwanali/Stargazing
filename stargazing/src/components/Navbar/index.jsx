0f0461a20121d149091a4a37a066c1852f76fb43
workuseifu1 <workuseifu1@gmail.com>
2023-11-04 19:06:12 -0500

    bug fix

--- stargazing/src/components/Navbar/index.jsx
+++ stargazing/src/components/Navbar/index.jsx
@@ -42,39 +42,4 @@ const Navbar = () => {
   );
 };
 
-//   return (
-//     <Layout className="layout">
-//       <Header
-//         style={{
-//           display: "flex",
-//           alignItems: "left",
-//         }}
-//       >
-//         {/* <div className="logo-container">
-//           <img src={logo} />
-//         </div> */}
-//         <div>
-//           <img className="logo" src={logo} />
-//         </div>
-//         <Menu
-//           theme="dark"
-//           mode="horizontal"
-//           defaultSelectedKeys={["1"]}
-//           items={new Array(4).fill(null).map((_, index) => {
-//             const key = index + 1;
-//             const labelListLoggedIn = ["Home", "Saved", "Profile", "Logout"];
-//             const labelList = ["Home", "Saved", "Signup"];
-//             //if logged in
-//             //TODO:if not logged in
-//             return {
-//               key,
-//               label: labelListLoggedIn[key - 1],
-//             };
-//           })}
-//         />
-//       </Header>
-//     </Layout>
-//   );
-// };
-
 export default Navbar;
