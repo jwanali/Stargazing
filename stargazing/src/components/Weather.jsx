64713bee982f5c53f262833c62c60d3212bbdd3a
workuseifu1 <workuseifu1@gmail.com>
2023-11-05 19:32:29 -0600

    update

--- stargazing/src/components/Weather.jsx
+++ stargazing/src/components/Weather.jsx
@@ -1,15 +1,4 @@
 import React from "react";
-
-
-
-// const Weather = (props) => {
-//   console.log(props.Weather)
-//   return (
-//     <div>{props.weather.latitude}</div>
-//   )
-// }
-
-// export default Weather;
 import { useState } from 'react';
 import Search from "./weather/search";
 import CurrentWeather from "./weather/current-weather";
