c15fd5246989e0f91d28b23afd8d34c6207f0800
workuseifu1 <workuseifu1@gmail.com>
2023-11-06 22:52:52 -0600

    Login

--- stargazing/src/components/Login.jsx
+++ stargazing/src/components/Login.jsx
@@ -1,64 +1,124 @@
+// import React, { useState } from "react";
+// import useApplicationData from "../hook/useApplicationData";
+// import Alert from "./Alert";
+// import "../styles/Login.css";
+// export default function Login() {
+//   const { login, isAuthenticated, error } = useApplicationData();
+//   const [state, setState] = useState({
+//     email: "",
+//     password: "",
+//   });
+//   // const history = useHistory();
+
+//   const handleSubmit = async(e) => {
+//     e.preventDefault();
+
+//     const loginResult = await onLogin(state);
+//     props.onLogin(state);
+//     if(loginResult.success) {
+//       history.push("/home");
+//     } else {
+//       history.push("/signup");
+//     }
+      
+//   }
+
+//   return (
+//     <form
+//       onSubmit={(e) => {
+//         e.preventDefault();
+//         props.onLogin(state);
+//       }}
+//     >
+//       {props.message && (
+//         <Alert message={props.message} messageType={props.messageType} />
+//       )}
+//       <div className="login">
+//         <h1>Login</h1>
+//         <label>
+//           <input
+//             type="text"
+//             placeholder="email"
+//             name="email"
+//             value={formData.email}
+//             onChange={handleInputChange}
+//           />
+//         </label>
+//         <label>
+//           <input
+//             type="password"
+//             placeholder="password"
+//             name="password"
+//             value={formData.password}
+//             onChange={handleInputChange}
+//           />
+//         </label>
+//         <div>
+//           <button type="submit">Login</button>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// }
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import {onSignUp, onLogin, message,messageType, onCreateEvent } from "../hook/useApplicationData";
+import useApplicationData from "../hook/useApplicationData";
 import Alert from "./Alert";
+import { Navigate } from "react-router-dom";
 import "../styles/Login.css";
+
 export default function Login(props) {
-  const [state, setState] = useState({
+  const { onLogin, message, messageType } = useApplicationData();
+  const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
-  // const history = useHistory();
 
-  // const handleSubmit = async(e) => {
-  //   e.preventDefault();
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-  //   const loginResult = await onLogin(state);
-  //   props.onLogin(state);
-  //   if(loginResult.success) {
-  //     history.push("/home");
-  //   } else {
-  //     history.push("/signup");
-  //   }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const loginResult = await onLogin(formData);
+    if (loginResult.success) {
+      return <Navigate to="/" />
+    } else {
+       return <Navigate to="/Signup" />
       
-  // }
+    }
+  };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        props.onLogin(state);
-      }}
-    >
-      {props.message && (
-        <Alert message={props.message} messageType={props.messageType} />
-      )}
-      <div className="login">
-        <h1>Login</h1>
-        <label>
-          <input
-            type="text"
-            placeholder="email"
-            name="email"
-            onChange={(e) => {
-              setState({ ...state, email: e.target.value });
-            }}
-          />
-        </label>
-        <label>
-          <input
-            type="password"
-            placeholder="password"
-            name="password"
-            onChange={(e) => {
-              setState({ ...state, password: e.target.value });
-            }}
-          />
-        </label>
-        <div>
-          <button type="submit">Login</button>
+    <div>
+      <form onSubmit={handleSubmit}>
+        {message && (
+          <Alert message={message} messageType={messageType} />
+        )}
+        <div className="login">
+          <h1>Login</h1>
+          <label>
+            <input
+              type="text"
+              placeholder="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </label>
+          <div>
+            <button type="submit">Login</button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
--- stargazing/src/hook/useApplicationData.js
+++ stargazing/src/hook/useApplicationData.js
@@ -7,104 +7,118 @@ export const ACTIONS = {
   SAVE_EVENT: "SAVE_EVENT"
 };
 
- const reducer = (state, action) => {
-  
-   switch (action.type) {
-    case ACTIONS.SAVE_EVENT:
-      return {...state,message:action.payload,messageType:action.messageType}
-    case ACTIONS.USER_SIGNUP: 
-    
-    return { ...state, message: action.payload, messageType: action.messageType };    
-    case ACTIONS.USER_LOGIN:
-      return {...state,photoData: action.payload}
-   }
- };
- const initialState = {
+const reducer = (state, action) => {
 
- };
+  switch (action.type) {
+    case ACTIONS.SAVE_EVENT:
+      return { ...state, message: action.payload, messageType: action.messageType }
+    case ACTIONS.USER_SIGNUP:
+
+      return { ...state, message: action.payload, messageType: action.messageType };
+    case ACTIONS.USER_LOGIN:
+      return {
+        ...state,
+        user: action.user,
+        isAuthenticated: true,
+        error: null,
+      };
+  }
+};
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+  error: null,
+};
 
 export default function useApplicationData() {
- 
- 
- const [state, dispatch] = useReducer(reducer, initialState);
 
- const onCreateEvent = (data) => {
-  const options = {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-     body: JSON.stringify(data),
-  };
 
-  fetch(`http://localhost:8080/add_event`,options)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onCreateEvent = (data) => {
+    const options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch(`http://localhost:8080/add_event`, options)
       .then((res) => {
-               
-        return  res.json();
+
+        return res.json();
       })
       .then((result) => {
-        
+
         dispatch({
           type: ACTIONS.SAVE_EVENT,
           payload: result.message || result.error,
-          messageType: result.message ? "success":"error", 
-        });}
-        
+          messageType: result.message ? "success" : "error",
+        });
+      }
+
       )
-      .catch(err=> alert("An error Occured."  + err))
+      .catch(err => alert("An error Occured." + err))
   };
 
- 
- const onSignUp = (data) => {
- 
- 
- const options = {
-   method: "POST",
-   mode: "cors",
-   headers: {
-     "Content-Type": "application/json",
-   },
-   body: JSON.stringify(data),
- };
 
-  fetch(`http://localhost:8080/sign_up`,options)
+  const onSignUp = (data) => {
+
+
+    const options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch(`http://localhost:8080/sign_up`, options)
       .then((res) => {
-               
-        return  res.json();
+
+        return res.json();
       })
       .then((data) => {
-        
-        dispatch({ type: ACTIONS.USER_SIGNUP, payload: data.message , messageType:"success"})}
-        
+
+        dispatch({ type: ACTIONS.USER_SIGNUP, payload: data.message, messageType: "success" })
+      }
+
       )
-      .catch(err=> alert("An error Occured."  + err))
+      .catch(err => alert("An error Occured." + err))
   };
 
-    const onLogin = (userdata) => {
-       const options = {
-         method: "POST",
-         mode: "cors",
-         headers: {
-           "Content-Type": "application/json",
-         },
-         body: JSON.stringify(userdata),
-       };
-
-      fetch(`http://localhost:8080/login`, options)
-        .then((res) => {
-           if (!res.ok) {
-             return res.text().then((text) => {
-               throw new Error(text);
-             });
-           }
-          res.json();
-        })
-        .then((data) => dispatch({ type: ACTIONS.USER_LOGIN, payload: data }))
-        .catch((err) => alert("An error Occured." + err));
+  const onLogin = (userdata) => {
+    const options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userdata),
     };
-const message = state.message;
-const messageType = state.messageType;
+
+    return fetch(`http://localhost:8080/login`, options)
+      .then((res) => {
+        if (!res.ok) {
+          return res.text().then((text) => {
+            throw new Error(text);
+          });
+        }
+        return res.json();
+      })
+      .then((data) => {
+        dispatch({ type: ACTIONS.USER_LOGIN, user: data });
+        return { success: true };
+      })
+      .catch((err) => {
+        return { success: false, error: "An error occurred." + err };
+      });
+  };
+  const message = state.message;
+  const messageType = state.messageType;
 
   return {
     onSignUp,
